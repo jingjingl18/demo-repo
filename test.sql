3bd4d53110263c5e37019a4f05c5955e71ac861f
Jingjing Li <Jingjing.Li@resmed.com.sg>
2022-02-19 13:18:06 +0800

    Revert "revert"
    This reverts commit d39d924827c77dfcf07f5e86afdf6bc262f1480f.
    
    revert back

--- test.sql
+++ test.sql
@@ -1,9 +1,5 @@
 select * from table limit 10
 where age >= 18
-<<<<<<< HEAD
 and gender == 'Male'
 and setup_mask == 'nasal'
-=======
->>>>>>> parent of 6df20e7 (adjust order)
 and setup_dt >= '2020-01-01'
-and gender == 'Male';
